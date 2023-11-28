f9fb66ce9e74fd3a4ba02ad5f5098d3c0f3a6a49
anoop <anoopano20@gmail.com>
2023-11-28 20:41:26 +0530

    create function to handle submit and add constants

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,18 +1,54 @@
 "use client"
 
 import { useState } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { API_URLS, MODES } from '@constants'
 import Form from '@components/Form'
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const {data: session} = useSession();
+
   const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState("")
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: [],
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch(API_URLS.CREATE_PRMOPT,
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            prompt: post.prompt,
+            tag: post.tag,
+            userId: session?.user.id,
+          })
+        }
+      );
+
+      if(response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.debug(error)
+    } finally {
+      setSubmitting(false);
+    }
+  }
+
   return (
     <Form
-        type="Create"
+        type={MODES.CREATE}
         post={post}
         setPost={setPost}
         submitting={submitting}
-        // handleSubmit={handleSubmit}
+        handleSubmit={handleSubmit}
     />
   )
 }
---
+++ constants.js
@@ -0,0 +1,9 @@
+export const API_URLS = {
+    CREATE_PRMOPT: "api/prompt/create-prompt",
+    EDIT_PRMOPT: "api/prompt/edit-prompt",
+}
+
+export const MODES = {
+    CREATE: "Create",
+    EDIT: "Edit",
+}
