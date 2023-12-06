f7d4baf7d49fee68e3d367458a7010ad1861afb4
anoop <anoopano20@gmail.com>
2023-12-06 23:21:12 +0530

    update edit prompt function

--- app/edit-prompt/page.jsx
+++ app/edit-prompt/page.jsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useState, useEffect } from 'react'
-import { useSession } from 'next-auth/react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { API_URLS, MODES } from '@constants'
 import Form from '@components/Form'
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -32,7 +32,23 @@ const MyProfile = () => {
   }
 
   const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure you want to delete the promot?");
 
+    if(hasConfirmed) {
+      try {
+        await fetch(API_URLS.GET_PROMPT(post._id), {
+          method: "DELETE"
+        });
+
+        const filteredPosts = posts.filter(p => 
+          p._id !== post._id
+        );
+        setPosts(filteredPosts);
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
   }
 
   return (
