0318c691958c70a3c1a5b7b933cd7dd1d2b676bd
anoop <anoopano20@gmail.com>
2023-12-11 23:21:27 +0530

    add detailed messages while establishing connection

--- utils/database.js
+++ utils/database.js
@@ -1,21 +1,26 @@
 import mongoose from 'mongoose';
 
-let isConnected = false;
+let isConnected = false; // track the connection
 
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true);
+  mongoose.set('strictQuery', true);
 
-    if(isConnected) {
-        return
-    }
+  if(isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
 
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: "share_prompt",
-            useNewUrlParser: true,
-        })
-        isConnected = true;
-    } catch (error) {
-        console.debug(error)
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
+    isConnected = true;
+
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error);
+  }
 }
