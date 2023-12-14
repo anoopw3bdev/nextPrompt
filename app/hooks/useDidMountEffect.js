259aab52068210426531418086dca798b31ade26
anoop <anoopano20@gmail.com>
2023-12-14 10:00:40 +0530

    created a custom hook which runs every time except first render

---
+++ app/hooks/useDidMountEffect.js
@@ -0,0 +1,15 @@
+import { useEffect, useRef } from "react";
+
+const useDidMountEffect = (callback, deps) => {
+    const didMount = useRef(false);
+
+    useEffect(() => {
+      if(didMount.current) {
+        callback();
+      } else {
+        didMount.current = true;
+      }
+    }, [...deps, callback])
+}
+
+export default useDidMountEffect
